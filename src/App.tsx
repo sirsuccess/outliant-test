443128cf77e5b8166be6699af835acdd101ceab7
sirsuccess <kanuamani@gmail.com>
2022-11-24 00:53:45 +0100

    footers

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Hero from './components/Hero';
+import './assets/fonts/KallistoBold.otf';
+import './assets/fonts/AeonikTRIAL-Regular.otf';
+import './sass/style.scss';
+import Service from './components/Service';
+import About from './components/About';
+import MarqueeText from './components/MarqueeText';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <MarqueeText />
+      <Service />
+      <About />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
