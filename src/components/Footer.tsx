ba55e9158404148ed4503ee570a70ac4e8cd3ce5
sirsuccess <kanuamani@gmail.com>
2022-11-24 01:42:39 +0100

    performance

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Logo from '../assets/icons/logo.svg';
-import { useForm } from 'react-hook-form';
-import Swal from 'sweetalert2';
+import React from "react";
+import Logo from "../assets/icons/logo.svg";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 type FormData = {
   email: string;
@@ -16,46 +16,46 @@ const Footer = () => {
 
   const onSubmit = (value: {}) => {
     const data = value;
-    console.log('data is ', data);
+    console.log("data is ", data);
 
     reset();
     Swal.fire({
-      icon: 'success',
-      title: 'Thanks...',
-      text: 'Your subscription has been received!',
-      confirmButtonColor: '#e4f22f',
+      icon: "success",
+      title: "Thanks...",
+      text: "Your subscription has been received!",
+      confirmButtonColor: "#e4f22f",
     });
   };
   return (
-    <footer className='footer-container'>
-      <a href='/' className='logo'>
-        <img src={Logo} alt=' webshift' className='l' />
+    <footer className="footer-container">
+      <a href="/" className="logo">
+        <img src={Logo} alt=" webshift" className="l" />
       </a>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='form-container'>
+        <div className="form-container">
           <input
-            type='email'
-            {...register('email', {
-              required: 'Email address required',
+            type="email"
+            {...register("email", {
+              required: "Email address required",
               pattern: {
                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'invalid email address',
+                message: "invalid email address",
               },
             })}
-            placeholder='Email address'
-            className=''
+            placeholder="Email address"
+            className=""
           />
-          <button type='submit'>JOIN OUR MOVEMENT</button>
+          <button type="submit">JOIN OUR MOVEMENT</button>
         </div>
-        <span className=''>{errors.email && errors.email.message}</span>
+        <span className="">{errors.email && errors.email.message}</span>
       </form>
 
-      <p className='copyright'>Copyright 2022 Webshift. All Rights Reserved</p>
+      <p className="copyright">Copyright 2022 Webshift. All Rights Reserved</p>
 
-      <div className='navigations'>
-        <a href='/'>Terms of Service</a>
-        <a href='/'>Privacy Policy</a>
-        <a href='/'>Cookie Policy</a>
+      <div className="navigations">
+        <a href="/">Terms of Service</a>
+        <a href="/">Privacy Policy</a>
+        <a href="/">Cookie Policy</a>
       </div>
     </footer>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import Logo from '../assets/icons/logo.svg';
+import React from "react";
+import Logo from "../assets/icons/logo.svg";
 const Hero = () => {
   return (
-    <section className='hero'>
-      <a href='/' className='logo'>
-        <img src={Logo} alt=' webshift' className='l' />
+    <section className="hero">
+      <a href="/" className="logo">
+        <img src={Logo} alt=" webshift" className="l" />
       </a>
-      <div className='hero-details'>
-        <h1 className='title'>
+      <div className="hero-details">
+        <h1 className="title">
           The easiest <br /> way to build XR websites.
         </h1>
-        <p className='description'>
+        <p className="description">
           Webshift empowers designers and developers to build immersive AR/VR
-          websites in a completely visual canvas - no coding needed.{' '}
+          websites in a completely visual canvas - no coding needed.{" "}
         </p>
       </div>
     </section>
   );
-}; 
+};
 
 export default Hero;
