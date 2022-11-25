a4f475fb0a1d33670f644d42db138d3ffe3909cc
sirsuccess <kanuamani@gmail.com>
2022-11-25 12:19:15 +0100

    implement AOS animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect} from "react";
 import Hero from './components/Hero';
 import './assets/fonts/KallistoBold.otf';
 import './assets/fonts/AeonikTRIAL-Regular.otf';
@@ -7,8 +7,15 @@ import Service from './components/Service';
 import About from './components/About';
 import MarqueeText from './components/MarqueeText';
 import Footer from './components/Footer';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <>
       <Hero />
