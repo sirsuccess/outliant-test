8d2f340ce4580d0a284ed198646e3d231a5d8159
sirsuccess <kanuamani@gmail.com>
2022-11-25 12:20:02 +0100

    implement AOS animation

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -6,28 +6,28 @@ const Service = () => {
     <section className="service_container">
       <div className="service-row">
         <div className="diamond" />
-        <div className="empty-divs"></div>
+        <div className="empty-divs" ></div>
         <div className="empty-divs"></div>
         <div className="empty-divs"></div>
         <div className="v" />
         <div className="empty-divs"></div>
       </div>
       <div className="service">
-        <div className="service-left">
-          <img src={Ring} alt=" a ring" className="image" />
+        <div className="service-left" >
+          <img src={Ring} alt=" a ring" className="image" data-aos="flip-left" data-aos-duration="2000" />
         </div>
         <div className="service-right">
-          <h1 className="title">
+          <h1 className="title" data-aos="zoom-in" data-aos-duration="2000">
             Design and build the future of the internet.
           </h1>
-          <p className="description">
+          <p className="description" data-aos="fade-up" data-aos-duration="2000">
             We believe that in the near future every website will have an AR/VR
             experience (Responsive XR; Responsive Reality) — a near identical
             adoption we saw with smartphones and responsive web. We’re here to
             accelerate this adoption by making building websites in XR
             intuitive, easy, and fun.
           </p>
-          <button className="join-button">JOIN OUR MOVEMENT</button>
+          <button className="join-button" data-aos="fade-up" data-aos-duration="2000">JOIN OUR MOVEMENT</button>
         </div>
       </div>
       <div className="ball-container">
